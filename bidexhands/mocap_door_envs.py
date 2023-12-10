correct rotation matric

bidexhands/mocap_door_envs.py
@@ -120,8 +120,8 @@ class isaac():
         action[0:3] = z_rot @ action[0:3]
 
 
-        action[3], action[4] = action[4], action[3]
-        action[3] = -1 * action[3]
+        # action[3], action[4] = action[4], action[3]
+        # action[3] = -1 * action[3]
         # action[4] += 0.5
         action[5] = action[5] + np.pi/2
         print("action[5]: ", action[5]/np.pi )

bidexhands/mocap_test.py
@@ -78,51 +78,22 @@ class isaac():
         if( self.count == 1): # initialize (x,y,z)
             self.init_pos =  action[0:3].copy()
         action[0:3] = action[0:3] - self.init_pos
-        # if(self.count % 5 != 0 ): # freq control
-        #     return
-        
-        # action = [ 0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0495,  1.5710,
-        #   1.5710,  1.1574, -0.1480,  1.5710,  1.5710,  1.0553, -0.2646,  1.5710,
-        #   1.5710,  1.1953,  0.0624, -0.2138,  1.5710,  1.5587,  1.3762,  0.3513,
-        #   0.3093,  0.1722,  0.3505, -0.2404]
-        # action = [ 0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000, 
-        #     -0.349,   0.9305,  1.571,   1.571,
-        #     -0.3489,  1.2059,  1.571,   1.5513,
-        #     -0.349,   0.9256,  1.571,   1.571,
-        #     7.8775e-05, -3.4897e-01,  3.4559e-01,  1.5710e+00,  1.5710e+00,
-        #     0.0129,  0.3373,  0.1,     0.4326, -0.4756]
-        # action = [ 0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.0000,
-        #   0.0,  0.0,  0.0,
-        #   0.0,  0.0,  0.0,  
-        #   0.0,  0.0,  0.0,  
-        #   0.0,  0.0,  0.0,  
-        #   0.0,  0.0,  0.0,  0.0,  0.0,
-        #   0.3513, 0.3093,  0.1722,  0.3505, -0.2404]
-        # action = [ 0.0000,  1.0000,  0.0000,  0.0000,  0.0000,  0.0000,        
-        #     0.0, 0.0, 0.0, 0.0,
-        #     0.0, 0.0, 0.0, 0.0,
-        #     0.0, 0.0, 0.0, 0.0,
-        #     0.0, 0.0, 0.0, 0.0, 0.0,
-        #     0.0, 0.0, 0.0, 0.0, 0.0]
-        # action = np.array( action )
         
         root_pos = action[:6].copy()
         print("\n")
         print("new iter root_pose: ", root_pos)
-        #action[0] = -1 * action[0]
         action[0] = -1 * action[0]
         action[1] = -1 * action[1]
-        action[3] = -1 * action[3]
-        action[4] = -1 * action[4] 
-        # action[3], action[4] = action[4], action[3]
-        if action[0] < 0.0:
-            action[0] = action[0]*3
-        action[2] = action[2]*2
-                
+
+        #action[3], action[5] = action[5], action[3]
+
+        # action[5] = -1 * action[5]
+        action[4] = -1 * action[4]  
+        action[3] = -1 * action[3]  
         # # door env setting
         # action[0] = -1 * action[0]
         # action[1] = -1 * action[1]
-        # action[3] = -1 * action[3]
+        
         # action[4] = -1 * action[4]        
         # action[0:3] = z_rot @ action[0:3]
         # action[3], action[4] = action[4], action[3]

bidexhands/tasks/mocap_shadow_hand_swing_cup.py
@@ -1524,7 +1524,7 @@ def compute_hand_reward(
     successes = torch.where(successes == 0, 
                     torch.where(rot_dist < 0.785, torch.ones_like(successes), successes), successes)
 
-
+    max_episode_length = 1000000000
     resets = torch.where(progress_buf >= max_episode_length, torch.ones_like(resets), resets)
 
     goal_resets = torch.zeros_like(resets)

mocap_right_full.py
@@ -377,13 +377,15 @@ class isaac():
         #zeros = np.zeros((6,))
         action[0] = -1 * action[0]
         action[1] = -1 * action[1]
-        action[3] = -1 * action[3]
+
+        action[3], action[5] = action[5], action[3]
+
+        action[5] = -1 * action[5]
         action[4] = -1 * action[4]        
         
         # action[0:3] = z_rot @ action[0:3]
-        action[3], action[4] = action[4], action[3]
+        # 
         # action[3] = -1 * action[3]
-        # action[4] += 0.2
         # action[5] = action[5] + np.pi/2
 
         print("action[3:6]: ", action[3:6])