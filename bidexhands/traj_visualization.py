6d6817e7085afe1f321ea56cb075c9bd267e8d3d
jiahexu <xjh4438318846@gmail.com>
2023-12-23 22:56:55 -0500

    fix coord of door handle

--- bidexhands/traj_visualization.py
+++ bidexhands/traj_visualization.py
@@ -7,8 +7,8 @@ import rospy
 # easy
 # env_name = "MocapShadowHandDoorOpenInward" # right view
 # env_name = "MocapShadowHandDoorOpenOutward" # right view
-# env_name = "MocapShadowHandDoorCloseInward" # right view
-env_name = "MocapShadowHandDoorCloseOutward" # right view
+env_name = "MocapShadowHandDoorCloseInward" # right view
+# env_name = "MocapShadowHandDoorCloseOutward" # right view
 
 # env_name = "MocapShadowHandSwingCup" # right view
 # env_name = "MocapShadowHandLiftUnderarm" # right view
